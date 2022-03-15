6a24669d40b5b5e3f9103a0638ae8eca6f0557bb
unknown <bohara.harish.7965@gmail.com>
2022-03-15 09:08:03 +0545

    DigiMarketer -> Version 1.1 Removing email configuration

--- digitalmarketing/settings.py
+++ digitalmarketing/settings.py
@@ -132,11 +132,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# EMAIL Settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'mr.tester484@gmail.com'
-EMAIL_HOST_PASSWORD = 'ForTesting@#Testing'
-EMAIL_USE_TLS = True
